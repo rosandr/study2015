aec52c2465cbf09164a9f60609fff1f56e41345f
rosandr <lesik1975@mail.ru>
2015-09-22 12:50:20 +0300

    print PWD

--- 002/printpwd.c
+++ 002/printpwd.c
@@ -4,26 +4,16 @@
 int main(int argc, char** argv, char** env)
 {
     char* pwd = getenv( "PWD");
-    printf( "Curren directory: %s\n", pwd);
-
-
-
-
-    int nn;
-
+    printf( "Curren directory using getenv():\t%s\n", pwd);
 
     while (*env)
     {
-	//printf("%s\n", *env);
-	char ppwd[]="PWD=";
-	nn = strncmp( *env, ppwd, sizeof(ppwd) );
+	char* ppwd = "PWD=";
+	int nn = strncmp( *env, ppwd, sizeof(ppwd) );
 	
-	if ( nn == sizeof(ppwd))
-	    printf("Current dir %s: \n", (*env)+4);
-
+	if ( nn == 0)
+	    printf("\nCurrent directory using env:\t\t%s: \n", *env+4);
 	env++;
     }
-
-
     return 0;
 }
