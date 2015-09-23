00225bec621640582e3e46baa5de5b1d4de10d92
rosandr <lesik1975@mail.ru>
2015-09-23 14:20:55 +0300

    process TREE

--- 004/proctree.c
+++ 004/proctree.c
@@ -2,30 +2,55 @@
 #include <stdlib.h>
 #include <string.h>
 
+#define BUF_SIZE 80
 
-int main(int argc, char** argv, char** env)
+int getPPid( int pid )
 {
-    int procid=0;
+    int ret=-1;
+    char file_name[BUF_SIZE];
 
-    if (argc <2)
-	printf("use pid=0\n");
-    else
-	procid = atoi (argv[1]);
-\
-    char file_name[256];
-
-    sprintf(file_name, "/proc/%d/status", procid);
-    printf("%s\n", file_name );
-
+    sprintf(file_name, "/proc/%d/status", pid);
+    //printf("%s\n", file_name );
 
     FILE* fd = fopen(file_name, "r");
     if (fd == NULL)
+    {
 	perror("open file");
+	return -1;
+    }
 
-    char buff[2048];
-    size_t nn = fread( buff, 2048, 1, fd );
+    char buff[BUF_SIZE];
 
-//printf("%s\n", buff );
+    while( fgets( buff, BUF_SIZE, fd ) != NULL )
+    {
+        if ( strstr ( buff, "PPid:") == NULL)
+	    continue;
+	else
+	    ret= atoi (buff+5);
+	break;
+    }
+    fclose(fd);
+    return ret;
+}
 
+
+int main(int argc, char** argv, char** env)
+{
+    int pid=1, ppid=1;
+
+    if (argc <2)
+	printf("use pid=1\n");
+    else
+	pid = atoi (argv[1]);
+
+    printf("%d ", pid);
+
+    while( (ppid=getPPid(pid))>0)
+    {
+	printf(" -> %d", ppid);
+	pid=ppid;
+    }
+
+    printf("\n");
     return 0;
 }
