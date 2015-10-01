14c1bf3d898e18c4a8b0da61ac524ab562defd4c
rosandr <lesik1975@mail.ru>
2015-10-01 12:30:53 +0300

    myshell final

--- 005/myshell.cpp
+++ 005/myshell.cpp
@@ -3,47 +3,48 @@
 #include <string.h>
 
 #include <signal.h>
-//#include <unistd.h>
+#include <unistd.h>
 
-//#include <sys/types.h>
+//#include <sys/types.h> 
 //#include <sys/wait.h>
 
 #include <iostream>
 
 using namespace std;
 
-#define BUF_SIZE 80
+#define BUF_SIZE	256
 
 //-------------------------------------------------------------------------
 void sigint_handler(int sig)
 { 
-	//(void)sig; 
-	printf("\nbye-bye...\n"); 
+	printf("\nGoodbye...\n"); 
+	exit (0);
 }
  
-
 int main(int argc, char** argv, char** env)
 {
     static struct sigaction act;
 
     act.sa_handler = sigint_handler;
     sigaction(SIGINT, &act, NULL);		// ^C
-	
+
 	int rc=0;
 	char buf[BUF_SIZE];
-	char cmdline[BUF_SIZE];
+	char path[BUF_SIZE];
+	path[0]=0;
+
+	sprintf(path, "%s:%s", getenv("PATH"), getcwd(buf, BUF_SIZE));
+	setenv("PATH", path, 1);
 
 	do
 	{
 		printf("sh > ");
 		fflush (stdout);
 
-		if( !fgets(buf, BUF_SIZE, stdin) )	break;
-		
-		sprintf(cmdline, "/bin/bash -c \"%s\"", buf);
-		rc= system (cmdline);
+		if( !fgets(buf, BUF_SIZE, stdin) )	break;	// ^D
+		rc = system (buf);
 	}
-	while (!rc);
+	while (1);
 
 	return rc;	
 }
