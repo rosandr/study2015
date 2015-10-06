79f8d11d6d4dae7e1aefdae23bcfb5894402d797
rosandr <lesik1975@mail.ru>
2015-10-06 12:28:42 +0300

    integral y=x

--- 007/summ.cpp
+++ 007/summ.cpp
@@ -14,13 +14,21 @@ using namespace std;
 typedef struct param
 {
     double left;
-    double righg;
+    double right;
+    double result;
+    pthread_t* thread;
 }Param;
+
 
 
 void* run(void* arg)
 {
-    sleep(5);
+    //sleep(1);
+	Param* par = (Param*)arg;
+	double delta = (par->right-par->left);
+	double abs = (par->right+par->left)/2;
+	par->result= delta*abs;	// y=x
+	//printf("%e  %e  %e\n",par->left, par->right, par->result);
     return 0;
 }
 
@@ -28,50 +36,67 @@ void* run(void* arg)
 void usage (char* name)
 {
     printf("\nUsage:\t%s N n\n", name);
-    printf("\tN - period, [1-64],\n\tn - sample number [1-64].\n");
+    printf("\tN - interval, [1-64],\n\tn - sample number [1-64].\n");
 }
 
 
 int main(int argc, char** argv, char** env)
 {
-    pthread_t thread;
-    int ret;
-    
     if (argc <3)
     {
 	usage(basename(argv[0]));
 	return 0;
     }
 
-    int period = atoi (argv[1]);
+    int interval = atoi (argv[1]);
     int sample = atoi (argv[2]);
 
-    if ((period <= 0) || (sample <= 0))
+    if ((interval <= 0) || (sample <= 0))
+    {
+		usage(basename(argv[0]));
+		return 0;
+    }
+
+    if ((interval > 64) || (sample > 64))
     {
 	usage(basename(argv[0]));
 	return 0;
     }
 
-    if ((period > 64) || (sample > 64))
-    {
-	usage(basename(argv[0]));
-	return 0;
-    }
+	Param* paramList[64]; 
+    int ret;
 
+	for (int i=0; i<sample; i++)
+	{
+    	pthread_t* thread = new pthread_t;
+	paramList[i]= new Param;
+	paramList[i]->left=((double)interval/sample)*i;
+	paramList[i]->right=((double)interval/sample)*(i+1);
 
-    //Param* param = new Param;
-    Param param;
-    ret = pthread_create(&thread, NULL, run, &param);
-    if(ret)
-    {
-	errno=ret;
-	perror("pthread_create");
-	return -1;
-    }
+    	ret = pthread_create(thread, NULL, run, paramList[i]);
+    	if(ret)
+    	{
+		errno=ret;
+		perror("pthread_create");
+		return -1;
+    	}
+    		//pthread_join(thread, NULL);
+		paramList[i]->thread=thread;
+	}
 
-    pthread_join(thread, NULL);
+	for (int i=0; i<sample; i++)
+		pthread_join( *(paramList[i]->thread), NULL);
 
+	double totalSum=0;
+	for (int i=0; i<sample; i++)
+	{
+		totalSum+=paramList[i]->result;
+		delete paramList[i]->thread;
+		delete paramList[i];
+	}
 
-    cout << endl;
+	cout << "function Y=X, interval [0-" << interval << "], sample count=" <<sample <<endl;
+	cout << "integral="<<totalSum <<endl;
+
     return 0;
 }
