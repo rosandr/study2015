f0ca2a8c108772acbd71e20a1e8984296b8426b0
rosandr <lesik1975@mail.ru>
2015-10-07 11:35:35 +0300

    tie-tie

--- 009/tie-tie.cpp
+++ 009/tie-tie.cpp
@@ -11,18 +11,48 @@
 using namespace std;
 #define BUF_SIZE 256
 
+
+void usage (char* name)
+{
+    printf("\nUsage:\t%s string filename\n", name);
+    printf("\tstring   - template to find\n");
+    printf("\tfilename - file to store result\n");
+    printf("Example: ls -l|%s rwx file.rwx\n", name);
+}
+
+
 int main(int argc, char** argv, char** env)
 {
+    if (argc <3)
+    {
+	usage(basename(argv[0]));
+	return 0;
+    }
+
+    FILE* fd = fopen(argv[2], "w");
+    if(fd == NULL)
+    {
+	perror("fopen");
+	exit(EXIT_FAILURE);
+    }
+
+    int count=0;
     char buf[BUF_SIZE];
     while( fgets(buf, BUF_SIZE, stdin) != NULL )
     {
         fputs(buf, stdout);
+	char* from = buf;
+	from = strstr( from, argv[1] );
 
-	for(int i=1; i<argc; i++)
+	while(from != NULL)
 	{
-	    printf("%s\n", argv[i]);
-
+	    count++;
+	    from++;
+	    from = strstr( from, argv[1] );
 	}
     }
+
+    fprintf (fd, "%s was found %d time(s)", argv[1], count);
+    fclose(fd);
     return 0;
 }
