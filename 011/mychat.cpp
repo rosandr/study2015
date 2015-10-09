47b45e2cef74e70bf2e693278844ce08200204b2
rosandr <lesik1975@mail.ru>
2015-10-09 15:27:52 +0300

    fifo chat

--- 011/mychat.cpp
+++ 011/mychat.cpp
@@ -2,101 +2,151 @@
 #include <stdlib.h>
 #include <string.h>
 #include <unistd.h>
+#include <fcntl.h>
 #include <error.h>
 #include <errno.h>
 #include <sys/types.h>
 #include <sys/stat.h>
-
+#include <sys/wait.h>
 
 #include <iostream>
 using namespace std;
 #define BUF_SIZE 256
 
-
 void usage (char* name)
 {
-    printf("\nUsage:\t%s [pipein] [pipeout]\n", name);
+    printf("\nUsage:\t%s pipein pipeout\n", name);
     printf("\tpipein  - input named pipe\n");
     printf("\tpipeout - output named pipe\n");
-    printf("Example: ./%s [inp] [outp]\n", name);
+    printf("Example: ./%s ff1 ff2\n", name);
 }
-
 
 int main(int argc, char** argv, char** env)
 {
-    if (argc == 2)
+    int fifo1, fifo2;
+    pid_t pid1, pid2;
+
+    if (argc < 3)
     {
 	usage(basename(argv[0]));
 	return 0;
     }
 
-    char pinDefName[] = "pipin";
-    char poutDefName[]= "pipout";
+    char* topin =argv[1];
+    char* topout=argv[2];
 
-    char* topin = pinDefName;
-    char* topout = poutDefName;
+    if (access(topin, F_OK) == -1)
+	fifo1=mkfifo(topin,  O_RDWR|S_IFIFO|S_IRWXU|S_IRWXG|S_IRWXO);
+    if (access(topout, F_OK) == -1)
+	fifo2=mkfifo(topout, O_RDWR|S_IFIFO|S_IRWXU|S_IRWXG|S_IRWXO);
 
-    if (argc == 3)
+    switch(pid1=fork())
     {
-	topin =argv[1];
-	topout=argv[2];
-    }
+	case -1:
+	    perror("fork1");
+	    exit(1);
+	case 0:
+//printf("%s\n", topin);
 
-    mkfifo(topin,  S_IFIFO|S_IRWXU|S_IRWXG|S_IRWXO);
-    mkfifo(topout, S_IFIFO|S_IRWXU|S_IRWXG|S_IRWXO);
+	    int fd = open(topin, O_RDONLY );		// reading pipe
 
+    	    if(fd <0)
+	    {
+		perror("fopen1");
+		exit(EXIT_FAILURE);
+	    }
+//printf("fopen1\n");
 
-    FILE* fdpin = fopen(topin,  "r");		// reading pipe
-    FILE* fdpout= fopen(topout, "w");		// writing pipe
+	    char buf[BUF_SIZE];
+	    buf[0]=0;
 
-    if( (fdpin == NULL)|| (fdpout == NULL))
-    {
-	perror("fopen");
-	exit(EXIT_FAILURE);
-    }
-
-    char buf[BUF_SIZE];
-    fgets(buf, BUF_SIZE, stdin);
-
-    printf("%s\n",buf);
-
-
-
-
-
-
+	    while(1)
+	    {
+		//fputs(buf, stdout);
+		//write(1, buf, sizeof(buf));
+		int n = read(fd, buf, BUF_SIZE);
+		if (n<=0)	break;
+		buf[n]=0;
+		fprintf(stdout, ">> %s\n",buf);
+	    }
+	    close(fd);
 
 /*
-    while(1)
-    {
+	    FILE* fd = fopen(topin,  "r");		// reading pipe
 
+	    if(fd == NULL)
+	    {
+		perror("fopen1");
+		exit(EXIT_FAILURE);
+	    }
 
+printf("fopen1");
 
+	    char buf[BUF_SIZE];
+	    while( fgets(buf, BUF_SIZE, fd) !=NULL )
+		puts(buf);
 
-	
-    }
-
-
-
-    int count=0;
-    char buf[BUF_SIZE];
-    while( fgets(buf, BUF_SIZE, stdin) != NULL )
-    {
-        fputs(buf, stdout);
-	char* from = buf;
-	from = strstr( from, argv[1] );
-
-	while(from != NULL)
-	{
-	    count++;
-	    from++;
-	    from = strstr( from, argv[1] );
-	}
-    }
-
-    fprintf (fd, "%s was found %d time(s)", argv[1], count);
+	    fclose(fd);
 */
-    fclose(fdpin);
-    fclose(fdpout);
+    }
+
+    switch(pid2=fork())
+    {
+	case -1:
+	    perror("fork2");
+	    exit(2);
+	case 0:
+//printf("%s\n", topout);
+
+	    int fd1 = open(topout, O_WRONLY );		// writing pipe
+
+	    if(fd1 <0)
+	    {
+		perror("fopen2");
+		exit(EXIT_FAILURE);
+	    }
+
+//printf("fopen2\n");
+
+	    char buf[BUF_SIZE];
+	    buf[0]=0;
+	    while( fgets(buf, BUF_SIZE, stdin) !=NULL )
+	    {
+		int n =write(fd1, buf , strlen(buf));
+		if(n<=0)	break;
+		//fprintf(stdout, "from f2%s %d\n",buf,  n);
+		//buf[0]=0;
+	    }
+	    close(fd1);
+
+/*
+	    FILE* fd1 = fopen(topout,  "w");		// writing pipe
+
+    	    if(fd1 == NULL)
+	    {
+		perror("fopen2");
+		exit(EXIT_FAILURE);
+	    }
+
+printf("fopen2\n");
+
+	    char buf[BUF_SIZE];
+	    //while( fgets(buf, BUF_SIZE, stdin) !=NULL )
+	    while(1)
+	    {
+		sleep(1);
+		//fputs(buf, fd);
+		//fputs("1234", fd);
+		//fputs("1234124", stdout);
+		int n=fprintf(fd1, "1234124123412341\n");
+		fprintf(stdout, "1234 %d\n",n);
+	    }
+	    fclose(fd1);
+*/
+    }
+
+    if( waitpid(pid1, &fifo1, 0)!=pid1 || waitpid(pid2, &fifo2, 0)!=pid2 )
+	perror("waitpid");
+
     return 0;
 }
