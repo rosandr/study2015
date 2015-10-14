a4450dbcd0bd93b341f37c5fcd8a589e94185526
rosandr <lesik1975@mail.ru>
2015-10-14 11:57:20 +0300

    broadcast udp chat

--- 014/bcastchat.cpp
+++ 014/bcastchat.cpp
@@ -11,45 +11,89 @@
 #include <sys/socket.h>
 #include <netinet/in.h>
 
+#include <pthread.h>
+
 #include <iostream>
 using namespace std;
 #define BUF_SIZE 256
 
 void usage (char* name)
 {
-	printf("\nUsage:\t%s localport remoteport\n", name);
+	printf("\nUsage:\t%s localport\n", name);
 	printf("\tlocalport  - local port number  [32675 - 64000],\n");
-	printf("\tremoteport - remote port number [32675 - 64000].\n");
-	printf("Example: ./%s 42443 42556  // in one xterm,\n", name);
-	printf("         ./%s 42556 42443  // in another xterm\n", name);
+	//printf("\tremoteport - remote port number [32675 - 64000].\n");
+	printf("Example: ./%s 42443   // in one host,\n", name);
+	printf("         ./%s 42443   // in another host\n", name);
 }
+
+typedef struct param
+{
+    int sock;
+    struct sockaddr_in* inaddr;
+    struct sockaddr_in* outaddr;
+}Param;
+
+
+pthread_t thread1, thread2;
+void sigint_handler(int sig) 
+{
+	pthread_cancel(thread1);
+	pthread_cancel(thread2);
+ }
+
+void* run1 (void* arg)
+{
+	Param* par = (Param*)arg;
+	
+	char buf[BUF_SIZE];
+	buf[0]=0;
+        sockaddr_in fromaddr;
+        socklen_t ln=sizeof(fromaddr);
+
+	printf("Please, type your message...\n");
+	while(1)
+	{
+        	int nb=recvfrom(par->sock, buf, BUF_SIZE, 0, (sockaddr*)&fromaddr, &ln);
+		if (nb <=0)	break;
+		buf[nb]=0;
+		printf(">> %s\n",buf);
+	}
+	return 0;
+}
+
+void* run2 (void* arg)
+{
+	Param* par = (Param*)arg;
+	
+	char buf[BUF_SIZE];
+	buf[0]=0;
+        while( fgets(buf, BUF_SIZE, stdin) !=NULL )
+	{
+		int n =sendto( par->sock, buf, BUF_SIZE, 0, (sockaddr*)(par->inaddr), sizeof(sockaddr_in));
+		if(n<=0)	break;
+	}
+	return 0;
+}
+
 
 int main(int argc, char** argv, char** env)
 {
-	unsigned short localport, remoteport;
-        pid_t pid1, pid2;
+	Param par;
+	unsigned short localport;
 
-	if (argc < 3)
+	if (argc < 2)
 	{
 	    usage(basename(argv[0]));
 	    return 0;
 	}
 
 	localport =  atoi(argv[1]);
-	remoteport = atoi(argv[2]);
-
 	if((localport<32675)||(localport>64000))
 		usage(basename(argv[0]));
 
-	if((remoteport<32675)||(remoteport>64000))
-		usage(basename(argv[0]));
-
-	if( localport==remoteport )
-	{
-		printf("\nPorts must differ!\n");
-		usage(basename(argv[0]));
-		exit(1);
-	}
+        static struct sigaction act;
+	act.sa_handler = sigint_handler;
+	sigaction(SIGINT, &act, NULL);		// ^C
 
 	//----------------------------------------------------------
 	int s=socket( AF_INET, SOCK_DGRAM, 0 );
@@ -60,62 +104,25 @@ int main(int argc, char** argv, char** env)
 	inaddr.sin_addr.s_addr = htonl((in_addr_t) INADDR_BROADCAST);
 	inaddr.sin_port = htons(localport);
 
-	struct sockaddr_in outaddr;
-	memset(&outaddr, 0, sizeof(outaddr));
-	outaddr.sin_family = AF_INET;
-	outaddr.sin_addr.s_addr = htonl((in_addr_t) INADDR_LOOPBACK);
-	outaddr.sin_port = htons(remoteport);
-
-
-	//----------------------------------------------------------
-    switch(pid1=fork())	
-    {
-	case -1:
-	    perror("fork1");
-	    exit(1);
-	case 0:
         while( bind(s, (struct sockaddr*)&inaddr, sizeof(inaddr)) < 0)
 	{
-    		printf(".");
+		printf(".");
 		fflush(NULL);
-    		usleep(100000);
+		usleep(100000);
 	}
 
-	char buf[BUF_SIZE];
-	buf[0]=0;
-        sockaddr_in fromaddr;
-        socklen_t ln=sizeof(fromaddr);
+	par.sock = s;
+	par.inaddr = &inaddr;
+//	par.outaddr = &outaddr;
 
-	printf("Please, type your message...\n");
+	//----------------------------------------------------------
+	pthread_create( &thread1, NULL, run1, &par);
+	pthread_create( &thread2, NULL, run2, &par);
 
-	while(1)
-	{
-        	int nb=recvfrom(s, buf, BUF_SIZE, 0, (sockaddr*)&fromaddr, &ln);
-		if (nb <=0)	break;
-		buf[nb]=0;
-		if( fromaddr.sin_port == ntohs(remoteport) )
-			printf(">> %s\n",buf);
-	}
-    }
+	void* res;
+	pthread_join( thread1, &res);
+	pthread_join( thread2, &res);
 
-    switch(pid2=fork())		// transmiter
-    {
-	case -1:
-		perror("fork2");
-		exit(2);
-	case 0:
-		char buf[BUF_SIZE];
-		buf[0]=0;
-		while( fgets(buf, BUF_SIZE, stdin) !=NULL )
-		{
-			int n =sendto( s, buf, BUF_SIZE, 0, (sockaddr*)&outaddr, sizeof(sockaddr_in));
-			if(n<=0)	break;
-		}
-    }
-
-    if( waitpid(pid1, &s, 0)!=pid1 || waitpid(pid2, &s, 0)!=pid2 )
-	perror("waitpid");
-
-    close(s);
-    return 0;
+	close(s);
+	return 0;
 }
