96821c88d468cbb73c50f3c695dcbf1400c671b8
rosandr <lesik1975@mail.ru>
2015-10-16 12:24:01 +0300

    static web server final

--- 016/websrv.cpp
+++ 016/websrv.cpp
@@ -11,8 +11,6 @@
 #include <sys/socket.h>
 #include <netinet/in.h>
 
-#include <pthread.h>
-
 #include <iostream>
 using namespace std;
 #define BUF_SIZE 3096
@@ -25,47 +23,78 @@ void usage (char* name)
 	printf("         ./%s 42443   // in another host\n", name);
 }
 
-typedef struct param
-{
-    int sock;
-    struct sockaddr_in* inaddr;
-    struct sockaddr_in* outaddr;
-}Param;
-
-pthread_t thread1;
+int sock;
 void sigint_handler(int sig) 
 {
-	pthread_cancel(thread1);
+    close(sock);
+    exit(0);
 }
 
-void* run1 (void* arg)
+int main(int argc, char** argv, char** env)
 {
-    int sock;
-    Param* par = (Param*)arg;
+    unsigned short localport;
 
-    char buf[BUF_SIZE];
-    //buf[0]=0;
-    sockaddr_in fromaddr;
-    socklen_t ln=sizeof(fromaddr);
+    if (argc < 2)
+    {
+        usage(basename(argv[0]));
+        return 0;
+    }
 
+    localport =  atoi(argv[1]);
+    if((localport<9999)||(localport>64000))
+	usage(basename(argv[0]));
+
+    static struct sigaction act;
+    act.sa_handler = sigint_handler;
+    sigaction(SIGINT, &act, NULL);		// ^C
+
+    //----------------------------------------------------------
+    int s=socket( AF_INET, SOCK_STREAM, 0 );
+
+    struct sockaddr_in inaddr;
+    memset(&inaddr, 0, sizeof(inaddr));
+    inaddr.sin_family = AF_INET;
+    inaddr.sin_addr.s_addr = htonl((in_addr_t) INADDR_ANY);
+    inaddr.sin_port = htons(localport);
+
+    while( bind(s, (struct sockaddr*)&inaddr, sizeof(inaddr)) < 0)
+    {
+	printf(".");
+	fflush(NULL);
+	usleep(100000);
+    }
+
+    if(listen(s, 10)<0)
+    {
+        perror("listen");
+        return 1;
+    }
+
+    //----------------------------------------------------------
     FILE* fd =  fopen("index.html", "r");
     FILE* fd1 = fopen("i.jpeg", "r");
-    
+
     if ((fd<0) || (fd1<0))
     {
         perror("open");
-        return 0;
+        return 1;
     }
-    
+
     fseek(fd, 0, SEEK_END);
     long fdsize = ftell(fd);
 
     fseek(fd1, 0, SEEK_END);
     long fd1size = ftell(fd1);
 
+    //----------------------------------------------------------
+    char buf[BUF_SIZE];
+
+    sockaddr_in fromaddr;
+    socklen_t ln=sizeof(fromaddr);
+
     while(1)
     {
-	sock = accept(par->sock, (sockaddr*)(&fromaddr), &ln);
+	sock = accept(s, (sockaddr*)(&fromaddr), &ln);
 	if( sock<= 0)
 	{
 	    perror("accept");
@@ -91,6 +120,7 @@ void* run1 (void* arg)
 	    to=stpcpy(to, "Content-Type: text/html; charset=UTF-8\n");
 	    to=stpcpy(to, "Keep-Alive: timeout=5,max=97\n");
 	    to=stpcpy(to, "\n");
+	    to=stpcpy(to, "Erorr 404 Page not found\n");
 	}
 	else
 	{
@@ -125,67 +155,12 @@ void* run1 (void* arg)
 	to=stpcpy(to, "\n");
 
 	send(sock, buf, to-buf, 0);
-	shutdown(sock, SHUT_RDWR);
+	//shutdown(sock, SHUT_RDWR);
 	close(sock);
     }
+    close(s);
     fclose(fd);
     fclose(fd1);
-    
+
     return 0;
-}
-
-int main(int argc, char** argv, char** env)
-{
-	Param par;
-	unsigned short localport;
-
-	if (argc < 2)
-	{
-	    usage(basename(argv[0]));
-	    return 0;
-	}
-
-	localport =  atoi(argv[1]);
-	if((localport<9999)||(localport>64000))
-		usage(basename(argv[0]));
-
-        static struct sigaction act;
-	act.sa_handler = sigint_handler;
-	sigaction(SIGINT, &act, NULL);		// ^C
-
-	//----------------------------------------------------------
-	int s=socket( AF_INET, SOCK_STREAM, 0 );
-
-	struct sockaddr_in inaddr;
-	memset(&inaddr, 0, sizeof(inaddr));
-	inaddr.sin_family = AF_INET;
-	inaddr.sin_addr.s_addr = htonl((in_addr_t) INADDR_ANY);
-	inaddr.sin_port = htons(localport);
-
-        while( bind(s, (struct sockaddr*)&inaddr, sizeof(inaddr)) < 0)
-	{
-		printf(".");
-		fflush(NULL);
-		usleep(100000);
-	}
-
-	if(listen(s, 10)<0)
-	{
-	    perror("listen");
-	    exit(1);
-	}
-
-	par.sock = s;
-	par.inaddr = &inaddr;
-//	par.outaddr = &outaddr;
-
-	//----------------------------------------------------------
-	pthread_create( &thread1, NULL, run1, &par);
-
-	void* res;
-	pthread_join( thread1, &res);
-
-//	shutdown(s, SHUT_RDWR);
-	close(s);
-	return 0;
 }
