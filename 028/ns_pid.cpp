87ac995024f873bfe7871fd4c13b78713f9f8832
rosandr <lesik1975@mail.ru>
2015-11-17 14:44:15 +0300

    namespace PID no work

--- 028/ns_pid.cpp
+++ 028/ns_pid.cpp
@@ -1,96 +1,144 @@
+/* multi_pidns.c
+
+   Copyright 2013, Michael Kerrisk
+   Licensed under GNU General Public License v2 or later
+
+   Create a series of child processes in nested PID namespaces.
+*/
 //#define _GNU_SOURCE
-#include <sys/wait.h>
-#include <sys/utsname.h>
 #include <sched.h>
-#include <string.h>
-#include <stdio.h>
-#include <stdlib.h>
 #include <unistd.h>
-#include <error.h>
-#include <errno.h>
-
-#include <sys/stat.h>
-#include <sys/types.h>
+#include <stdlib.h>
+#include <sys/wait.h>
+#include <string.h>
+#include <signal.h>
+#include <stdio.h>
+#include <limits.h>
 #include <sys/mount.h>
+#include <sys/types.h>
+#include <sys/stat.h>
 
-#include <iostream>
-using namespace std;
-#define BUF_SIZE 256
+/* A simple error-handling function: print an error message based
+   on the value in 'errno' and terminate the calling process */
 
-void usage (char* name)
+#define errExit(msg)    do { perror(msg); exit(EXIT_FAILURE); \
+                        } while (0)
+
+//#define STACK_SIZE (1024 * 1024)
+
+//static char child_stack[STACK_SIZE];    /* Space for child's stack */
+                /* Since each child gets a copy of virtual memory, this
+                   buffer can be reused as each child creates its child */
+
+/* Recursively create a series of child process in nested PID namespaces.
+   'arg' is an integer that counts down to 0 during the recursion.
+   When the counter reaches 0, recursion stops and the tail child
+   executes the sleep(1) program. */
+
+static int
+childFunc(int level)
 {
-    printf("\nUsage: %s <child-level>\n", name);
-}
+    static int first_call = 1;
+    //long level = (long) arg;
 
-char* newname;
+    if (!first_call) {
 
-int child_proc(int level)
-{
-    level--;
-    if(level<0)	return 0;
+        /* Unless this is the first recursive call to childFunc()
+           (i.e., we were invoked from main()), mount a procfs
+           for the current PID namespace */
 
+        char mount_point[PATH_MAX];
 
-    switch( pid=fork())		// transmiter
-    {
-	case -1:
-	    perror("fork");
-	    exit(EXIT_FAILURE);
+        snprintf(mount_point, PATH_MAX, "./multi_proc%c", (char) ('0' + level));
 
-	case 0:
-
-	if (unshare(CLONE_NEWPID) == -1)
-    	{
-	    perror("unshare");
-	    exit(EXIT_FAILURE);
-	}
-
+        mkdir(mount_point, 0555);       /* Create directory for mount point */
         if (mount("proc", mount_point, "proc", 0, NULL) == -1)
-        {
-	    perror("mount");
-	    exit(2);
-	}
-
-	char mount_point[BUF_SIZE];
-        sprintf(mount_point, "multi_proc%c", (char)('0'+level) );
-
-	child_proc();
-	execlp("sleep", "sleep", "600", (char *) NULL);
+            errExit("mount");
+        printf("Mounting procfs at %s\n", mount_point);
     }
+
+    first_call = 0;
+
+    if (level > 0)
+    {
+
+        /* Recursively invoke childFunc() to create another child in a
+           nested PID namespace */
+
+        level--;
+        pid_t child_pid;
+
+
+
+
+        switch( child_pid=fork())		// transmiter
+        {
+        case -1:
+            perror("fork");
+            exit(EXIT_FAILURE);
+
+        case 0:
+            if (unshare(CLONE_NEWPID) == -1)
+            {
+                perror("unshare");
+                exit(EXIT_FAILURE);
+            }
+/*
+            char mount_point[BUF_SIZE];
+            sprintf(mount_point, "multi_proc%c", (char)('0'+level) );
+
+            if (mount("proc", mount_point, "proc", 0, NULL) == -1)
+            {
+                perror("mount");
+                exit(EXIT_FAILURE);
+            }
+*/
+            childFunc(level);
+            //umount( mount_point);
+        }
+
+
+
+
+
+
+
+
+
+
+
+
+        /*
+        child_pid = clone(childFunc,
+                    child_stack + STACK_SIZE,
+                    CLONE_NEWPID | SIGCHLD, (void *) level);
+*/
+
+
+        if (waitpid(child_pid, NULL, 0) == -1)  /* Wait for child */
+            errExit("waitpid");
+
+    } else {
+
+        /* Tail end of recursion: execute sleep(1) */
+
+        printf("Final child sleeping\n");
+        execlp("sleep", "sleep", "1000", (char *) NULL);
+        errExit("execlp");
+    }
+
+    return 0;
 }
 
-
-int main(int argc, char** argv, char** env)
+int
+main(int argc, char *argv[])
 {
-    pid_t pid;
-    //struct utsname uts;
 
-    if (argc < 2) 
-    {
-	usage(basename(argv[0]));
-        exit(EXIT_FAILURE);
-    }
-    
+    int levels;
 
-    int level = atoi( argv[1]);
-    if(level > 5)
-    {
-	usage(basename(argv[0]));
-        exit(EXIT_FAILURE);
-    }
-
-
-
-
-    char mount_point[BUF_SIZE];
-
-    for(int i=0; i<level; i++)
-    {
-        sprintf(mount_point, "multi_proc%c", (char)('0'+i) );
-        mkdir(mount_point, 0555);
-    }
-
-
-    child_proc(level);
+    levels = (argc > 1) ? atoi(argv[1]) : 5;
+        printf("Caller PID = %ld, levels = %d\n",(long int)getpid(), levels);
+    childFunc(levels);
 
     exit(EXIT_SUCCESS);
 }
