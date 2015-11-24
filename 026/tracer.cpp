831bbbe7dafc563fed197ec8476d31abb3ffbf07
rosandr <lesik1975@mail.ru>
2015-11-24 15:55:04 +0300

    tracer no work

--- 026/tracer.cpp
+++ 026/tracer.cpp
@@ -12,12 +12,16 @@
 #include <netinet/in.h>
 #include <netdb.h>
 #include <arpa/inet.h>
+
 #include <netinet/ip_icmp.h>
+#include <netinet/ip.h>
+#include <netinet/udp.h>
 
 #include <getopt.h>
 #include <iostream>
 using namespace std;
 #define BUF_SIZE 1500
+#define REMOTE_PORT 3533
 
 extern int optind;
 
@@ -118,7 +122,10 @@ int main(int argc, char** argv, char** env)
         ip.s_addr += *(hp->h_addr);
     }
 
-    //printf("%x\n", ip.s_addr);
+    printf("%s\n", ipstr);
+    printf("%x\n", ip.s_addr);
+    printf("%x\n", inet_addr(ipstr));
+
 
     static struct sigaction act;
     act.sa_handler = sigint_handler;
@@ -132,7 +139,7 @@ int main(int argc, char** argv, char** env)
         perror("socket");
         return -1;
     }
-
+/**/
     bool opt=true;
     if(setsockopt (s, IPPROTO_IP, IP_HDRINCL, (char*)&opt, sizeof(opt))<0)
     {
@@ -144,80 +151,130 @@ int main(int argc, char** argv, char** env)
     memset(&addr, 0, sizeof(addr));
     addr.sin_family = AF_INET;
     addr.sin_addr.s_addr = ((in_addr_t)ip.s_addr);
-    addr.sin_port = htons(0);
+    addr.sin_port = htons( REMOTE_PORT );
 
+    char rd_buff[BUF_SIZE];
+    char wr_buff[BUF_SIZE];
+    char* ptr;
 
+    char msgHello[]="Hello to U, my friend";
+    struct iphdr ipHdr;
+    struct udphdr udpHdr;
 
-return 0;
+    //----------------------------------------------------------
+    // prepare ip header
+    u_int16_t totalSize = sizeof(udpHdr)+sizeof(ipHdr)+strlen(msgHello);
+    u_int16_t ipSize    = sizeof(ipHdr)/sizeof(unsigned long);
 
+    ipHdr.version = 4;
+    ipHdr.ihl = ipSize;
+    ipHdr.tos = 0;
+    ipHdr.tot_len = htons(totalSize);
+    ipHdr.id = 0;
+    ipHdr.frag_off = 0;
+    ipHdr.ttl = 1;
+    ipHdr.protocol = IPPROTO_UDP;
+    ipHdr.check = 0;
+    ipHdr.daddr = ip.s_addr;    //inet_addr(ipstr);
+    //ipHdr.saddr = inet_addr("");
 
+    //----------------------------------------------------------
+    // prepare udp header
+    u_int16_t udpSize = sizeof(udpHdr)+strlen(msgHello);
+    udpHdr.source = htons(3333);
+    udpHdr.dest = htons(REMOTE_PORT);
+    udpHdr.len = htons(udpSize);
+    udpHdr.check = 0;
 
+    //----------------------------------------------------------
+    // check summ
+    ptr = wr_buff;
+    memset(ptr, 0, sizeof(wr_buff));
+    u_int16_t udpChecksumSize = 0;
 
+    memcpy(ptr, &ipHdr.saddr, sizeof(ipHdr.saddr));
+    ptr += sizeof(ipHdr.saddr);
+    udpChecksumSize += sizeof(ipHdr.saddr);
 
+    memcpy(ptr, &ipHdr.daddr, sizeof(ipHdr.daddr));
+    ptr+= sizeof(ipHdr.daddr);
+    udpChecksumSize += sizeof(ipHdr.daddr);
 
+    ptr++;
+    udpChecksumSize += 1;
 
+    memcpy(ptr, &ipHdr.protocol, sizeof(ipHdr.protocol));
+    ptr += sizeof(ipHdr.protocol);
+    udpChecksumSize += sizeof(ipHdr.protocol);
 
-    // prepare ICMP echo request
-    struct icmphdr echoReq;
+    memcpy(ptr, &udpHdr.len, sizeof(udpHdr.len));
+    ptr += sizeof(udpHdr.len);
+    udpChecksumSize += sizeof(udpHdr.len);
 
-    memset(&echoReq, 0, sizeof(struct icmphdr));
-    echoReq.type = ICMP_ECHO;
-    echoReq.checksum = checkSum((u_int16_t *)&echoReq,
-            sizeof(struct icmphdr));
+    memcpy(ptr, &udpHdr, sizeof(udpHdr));
+    ptr += sizeof(udpHdr);
+    udpChecksumSize += sizeof(udpHdr);
 
-    printf("starting PING %s %d bytes of data\n", ipstr, sizeof(struct icmphdr));
+    for(int i = 0; i < strlen(msgHello); i++, ptr++)
+            *ptr = msgHello[i];
+    udpChecksumSize += strlen(msgHello);
+
+    udpHdr.check = checkSum((u_int16_t *)wr_buff, udpChecksumSize);
+
+    //----------------------------------------------------------
+    // prepare buffers
+    ptr = wr_buff;
+    memset(ptr, 0, sizeof(wr_buff));
+
+    memcpy(ptr, &ipHdr, sizeof(ipHdr));
+    ptr += sizeof(ipHdr);
+    memcpy(ptr,&udpHdr, sizeof(udpHdr));
+    ptr += sizeof(udpHdr);
+    memcpy(ptr, msgHello, strlen(msgHello));
+
 
     int nb;
+    nb =sendto( s, wr_buff, totalSize, 0,
+                   (sockaddr*)&addr, sizeof(sockaddr_in));
+
+
+    printf("total size=%d, nb=%d\n", totalSize, nb);
+
     fd_set rdfs;
     struct timeval tVal;
 
-    while(repeat--)
+    tVal.tv_sec = 1;
+    tVal.tv_usec = 0;
+
+    FD_ZERO(&rdfs);
+    FD_SET(s, &rdfs);
+
+    if ((nb = select(5, &rdfs, NULL, NULL, &tVal)) < 0)
     {
-        nb =sendto( s, (u_int8_t*)&echoReq, sizeof(struct icmphdr), 0,
-                       (sockaddr*)&addr, sizeof(sockaddr_in));
-        if(nb<=0)
+        perror("select");
+        return -1;
+    }
+
+    if (FD_ISSET(s, &rdfs))
+    {
+        //char buf[BUF_SIZE];
+        sockaddr_in fromaddr;
+        socklen_t ln=sizeof(fromaddr);
+
+
+        nb=recvfrom(s, rd_buff, BUF_SIZE, 0, (sockaddr*)&fromaddr, &ln);
+        if (nb <=0)
         {
-            perror("sendto");
+            perror("recvfrom");
             close(s);
             return -1;
         }
 
-        tVal.tv_sec = 1;
-        tVal.tv_usec = 0;
-
-        FD_ZERO(&rdfs);
-        FD_SET(s, &rdfs);
-
-        if ((nb = select(5, &rdfs, NULL, NULL, &tVal)) < 0)
+        //if(nb > 20)
         {
-            perror("select");
-            continue;
+            struct iphdr* p_ipHdr = (struct iphdr*)rd_buff;
+            printf("ttl=%d  (num bytes=%d)\n",p_ipHdr->ttl, nb);
         }
-
-        if (FD_ISSET(s, &rdfs))
-        {
-            char buf[BUF_SIZE];
-            sockaddr_in fromaddr;
-            socklen_t ln=sizeof(fromaddr);
-
-
-            nb=recvfrom(s, buf, BUF_SIZE, 0, (sockaddr*)&fromaddr, &ln);
-            if (nb <=0)
-            {
-                perror("recvfrom");
-                close(s);
-                return -1;
-            }
-
-            if(nb == sizeof(struct icmphdr)+20)
-            {
-                printf("reply from %s  (num bytes=%d) time=\?\?\?\n",ipstr, nb);
-                select(0, NULL, NULL, NULL, &tVal);
-                continue;
-            }
-            select(0, NULL, NULL, NULL, &tVal);
-        }
-        printf("packet lost...\n");
     }
 
     close(s);
