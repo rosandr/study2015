f710db2a1c714781679bbe73bf01d49399922580
rosandr <lesik1975@mail.ru>
2015-12-08 15:44:58 +0300

    test app

--- 030/app/kbuftest.cpp
+++ 030/app/kbuftest.cpp
@@ -19,16 +19,8 @@ using namespace std;
 #define BUF_SIZE 1024
 
 #include "../chardev.h"
-/*
+
 //=========================================================================
-void usage (char* name)
-{
-    printf("\nUsage:\t%s [ip] port\n", name);
-    printf("\tip  - server IPv4 address,\n");
-    printf("\tlocalport  - server port number  [10000 - 64000],\n");
-    printf("Example: ./%s 192.168.1.99 10000.\n", name);
-}
-*/
 void print_menu()
 {
     printf("\nenter your selection: \n");
@@ -41,109 +33,10 @@ void print_menu()
 }
 
 //-------------------------------------------------------------------------
-//int s;
-//struct sockaddr_in addr;
-//pthread_t thread;
-/*
-void sigint_handler(int sig)
-{
-    shutdown(s, SHUT_RDWR);
-    close(s);
-
-    pthread_cancel(thread);
-    pthread_join( thread, NULL);
-
-    exit(0);
-}
-
-void* run (void* arg)
-{
-    char buf[BUF_SIZE];
-    while(1)
-    {
-        int nb = recv(s, buf, BUF_SIZE, 0);
-        if ( nb>0 )
-        {
-            buf[nb]=0;
-            printf("%s\n", buf);
-        }
-        else
-        {
-            printf("connection refused\n");
-            break;
-        }
-    }
-
-    close(s);
-    printf("client thread exited\n");
-    return 0;
-}
-*/
 int main(int argc, char** argv, char** env)
 {
     int nb;
-    //struct in_addr ip;
-    //unsigned short port;
-/*
-    if (argc < 2)
-    {
-        usage(basename(argv[0]));
-        return 0;
-    }
-
-    if (argc < 3)
-    {
-        ip.s_addr = htonl(INADDR_ANY);
-        port =  atoi(argv[1]);
-    }
-    else
-    {
-        char* ipstr = argv[1];
-        port = atoi(argv[2]);
-
-        if (!inet_aton(ipstr, &ip))
-        {
-            printf("can't parse IP address %s\n", ipstr);
-            usage(basename(argv[0]));
-            return 0;
-        }
-    }
-
-    if((port<9999)||(port>64000))
-    {
-        printf("port range error: %d\n", port);
-        usage(basename(argv[0]));
-        return 0;
-    }
-
-    static struct sigaction act;
-    act.sa_handler = sigint_handler;
-    sigaction(SIGINT, &act, NULL);		// ^C
-
-    //----------------------------------------------------------
-    s=socket( AF_INET, SOCK_STREAM, 0 );
-
-    memset(&addr, 0, sizeof(addr));
-    addr.sin_family = AF_INET;
-    addr.sin_addr.s_addr = ((in_addr_t)ip.s_addr);
-    addr.sin_port = htons(port);
-*/
     char buf[BUF_SIZE];
-/*
-    int nb = connect(s, (sockaddr*)&addr, sizeof(sockaddr_in));
-    if(nb!=0)
-    {
-        perror("connect");
-        exit(1);
-    }
-
-    if( 0!=pthread_create( &thread, NULL, run, NULL) )
-    {
-        perror("pthread_create");
-        close(s);
-        return 0;
-    }
-*/
 
     char devfile_name[] = "/dev/kbuf";
     FILE* fd = fopen(devfile_name, "r+");
@@ -224,18 +117,6 @@ int main(int argc, char** argv, char** env)
         default:
             break;
         }
-        usleep(100000);
     }
-/*
-    //---------------------------------------------------------
-    void* res;
-    pthread_cancel(thread);
-    pthread_join( thread, &res);
-
-    if (res == PTHREAD_CANCELED)
-        printf(" thread was canceled\n");
-
-    close(s);
-*/
     return 0;
 }
